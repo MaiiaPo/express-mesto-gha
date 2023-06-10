51118b6993cf40745c96590543efc5ab36d8ab0e
Maiia <maiiapo.com@gmail.com>
2023-06-10 13:49:14 +0500

    Подключение к express и hot reload

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const express = require('express');
 
+const { PORT = 3000 } = process.env;
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
