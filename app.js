876d94794ef598778875491b665e8d2a44328815
Maiia <maiiapo.com@gmail.com>
2023-06-10 15:07:39 +0500

    Подключение к бд

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies */
 // eslint-disable-next-line import/no-extraneous-dependencies
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
 const { PORT = 3000 } = process.env;
 const app = express();
